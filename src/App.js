3f7d2c5aec48a06fae09bd96aecb47dd46c0ff57
ankitt01 <ankitrawat0102@gmail.com>
2021-12-25 23:19:14 +0530

    created the component structure of the project and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import './App.css';
-import {Switch, Router, Link} from 'react-router-dom'
+import {Switch, Router, Route, Link} from 'react-router-dom'
 import {Layout, Typography, Space} from 'antd'
-import Navbar from './components/Navbar';
+import {Navbar, Exchanges,Homepage,Cryptocurrencies,News,CryptoDetails} from './components';
 
 function App() {
   return (
     <div>
       <div className="navbar">
-        <Navbar />
+        <Navbar />  
       </div>
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
 
       <div className="footer">
