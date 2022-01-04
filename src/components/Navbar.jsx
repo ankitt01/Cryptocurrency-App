0720b18e7d248d19acbf9c65de02021b3ff69ca5
ankitt01 <ankitrawat0102@gmail.com>
2022-01-04 11:04:29 +0530

    Navbar component done styling and positioning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,9 +23,6 @@ function Navbar() {
                 <Menu.Item icon={<MoneyCollectOutlined />}>
                     <Link to="/exchanges">Exchanges</Link>
                 </Menu.Item>
-                <Menu.Item icon={<HomeOutlined />}>
-                    <Link to="/">Home</Link>
-                </Menu.Item>
                 <Menu.Item icon={<BulbOutlined />}>
                     <Link to="/news">News</Link>
                 </Menu.Item>
